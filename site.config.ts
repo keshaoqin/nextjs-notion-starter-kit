0013b713adabba684b81a2e8d6d6608a868192f2
keshaoqin <fmsbi@foxmail.com>
2023-03-31 18:22:44 +0800

    Update site.config.ts

--- site.config.ts
+++ site.config.ts
@@ -2,24 +2,24 @@ import { siteConfig } from './lib/site-config'
 
 export default siteConfig({
   // the site's root Notion page (required)
-  rootNotionPageId: '7875426197cf461698809def95960ebf',
+  rootNotionPageId: '3ba8ce65da9e4e57af675c15a0eea9eb?v=90e8839a741f4393aa4ba3cab7f8c1df',
 
   // if you want to restrict pages to a single notion workspace (optional)
   // (this should be a Notion ID; see the docs for how to extract this)
   rootNotionSpaceId: null,
 
   // basic site info (required)
-  name: 'Next.js Notion Starter Kit',
-  domain: 'nextjs-notion-starter-kit.transitivebullsh.it',
-  author: 'Travis Fischer',
+  name: 'Magic Prompt',
+  domain: 'https://wakeful-sled-9fd.notion.site/3ba8ce65da9e4e57af675c15a0eea9eb?v=90e8839a741f4393aa4ba3cab7f8c1df',
+  author: 'Muxiaoke GPT',
 
   // open graph metadata (optional)
   description: 'Example Next.js Notion Starter Kit Site',
 
   // social usernames (optional)
-  twitter: 'transitive_bs',
-  github: 'transitive-bullshit',
-  linkedin: 'fisch2',
+  twitter: '******',
+  github: '******',
+  linkedin: '******',
   // mastodon: '#', // optional mastodon profile URL, provides link verification
   // newsletter: '#', // optional newsletter URL
   // youtube: '#', // optional youtube channel name or `channel/UCGbXXXXXXXXXXXXXXXXXXXXXX`
